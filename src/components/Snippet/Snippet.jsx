4c3715e1da6bd8d8dc07c87aaf697dba94244c83
damkols <damolausmankolapo@gmail.com>
2022-02-13 18:29:28 -0800

    worked on snippet page and added variables and breakpoints

--- src/components/Snippet/Snippet.jsx
+++ src/components/Snippet/Snippet.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import Img from "../../assets/Snippet Image.png";
+import Img from "../../assets/Customizable Image.png";
+import "./Snippet.scss";
 
 const Snippet = () => {
   return (
@@ -7,7 +8,7 @@ const Snippet = () => {
       <div className="left">
         <h2>A fully customizable code snippet editor</h2>
         <p>
-          Fully customize the color scheme of your code snippet, or use our own
+          fully customize the color scheme of your code snippet, or use our own
           premade color schemes, Export your code snippets in JPG, PDF, PNG, or
           any of the other 10+ common extensions.
         </p>
@@ -16,7 +17,9 @@ const Snippet = () => {
         </button>
       </div>
       <div className="right">
-        <img src={Img} alt="" />
+        <div className="image">
+          <img src={Img} alt="" />
+        </div>
       </div>
     </div>
   );
