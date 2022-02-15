055ae19cf0af7e44ce8c393b4708191feae6a32a
damkols <damolausmankolapo@gmail.com>
2022-02-15 12:39:48 -0800

    built and styled Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,29 +1,44 @@
 import React from "react";
+import { useState } from "react";
 import logo from "../../assets/Logo.svg";
 import search from "../../assets/Search Icon.svg";
 import "./Navbar.scss";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className="nav_container">
-      <div className="logo">
+    <div className="navigation">
+      <div className="navLogo">
         <img src={logo} alt="" />
       </div>
-      <ul className="links">
+
+      <div className={`nav-items ${isOpen && "open"}`}>
         <li>
-          {" "}
-          <img src={search} alt="" />
+          <a onClick={() => setIsOpen(!isOpen)}>
+            <img src={search} alt="" />
+          </a>
         </li>
         <li>
-          {" "}
-          <a href="#">Sign in</a>
+          <a onClick={() => setIsOpen(!isOpen)}>Sign in</a>
         </li>
         <li>
-          <button>
-            Get started <span>- it's free</span>{" "}
-          </button>
+          <a onClick={() => setIsOpen(!isOpen)}>
+            <button>
+              {" "}
+              Get started <span>- it's free</span>
+            </button>
+          </a>
         </li>
-      </ul>
+      </div>
+
+      <div
+        onClick={() => setIsOpen(!isOpen)}
+        className={`btn-mobile ${isOpen && "open"}`}
+      >
+        <div className="btn-mobile-line"></div>
+        <div className="btn-mobile-line sm-line"></div>
+      </div>
     </div>
   );
 };
